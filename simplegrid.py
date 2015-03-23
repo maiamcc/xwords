2448e7c054795bfc2f2feed5bd0c4f7ccdd79d1e
Maia <maia.mcc@gmail.com>
2015-03-23 17:52:28 -0400

    use regex to find options for given word-start

--- simplegrid.py
+++ simplegrid.py
@@ -3,10 +3,12 @@
 from collections import namedtuple
 from copy import copy
 import string
+import re
 import random
 import nltk.corpus
 LETTERS = string.lowercase
 WORDS = nltk.corpus.words.words()
+# BLANK = "_" # character representing a blank
 AffectedEntry = namedtuple("AffectedEntry", ["wd_so_far", "old_wd_index", "new_wd_index"])
 # ^ represents the effect that adding a new word would have on a single entry. E.g.,
     # the word far is "B E _ _", the letter that would be changed in that word is the
@@ -35,7 +37,9 @@ def random_grid():
 # ...
 
 # Fill in acr, dwn, acr, down, acr, down
-# fourletter = [word for word in WORDS if len(word) == 4]
+global fourletter
+fourletter = [word for word in WORDS if len(word) == 4]
+
 row = ["_", "_", "_", "_"]
 g = [copy(row), copy(row), copy(row), copy(row)]
 
@@ -63,14 +67,30 @@ def get_entries_affected(grid, index, dir):
 
     return results
 
+def valid_wd_or_options_exist(xword_entry):
+    # or should i return the option list?
+    if str(xword_entry).lower() in fourletter:
+        return True
+    else:
+        pattern = "".join(xword_entry).replace("_", ".")
+        options = [word for word in fourletter if re.match(pattern, word)]
+        print xword_entry, "--", options
+        return bool(options)
+
+def check_fill_in(affected_list, new_word):
+    for affected in affected_list:
+        wd_to_check = affected.wd_so_far
+        wd_to_check[affected.old_wd_index] = new_word[affected.new_wd_index]
+        if not valid_wd_or_options_exist(wd_to_check):
+            print "Whoops, at least one word isn't valid or has no options:", "".join(wd_to_check)
+            return False
+    return True
+
 def fill_in_row(grid, y, word):
     """Place the given word in the row at index y."""
     grid[y] = [letter for letter in word]
 
 def fill_in_col(grid, x, word):
     """Place the given word in the column at index x."""
-    print_grid(grid)
     for i, row in enumerate(grid):
-        print i, row, word[i]
         row[x] = word[i]
-        print_grid(grid)
