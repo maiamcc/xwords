fe729a25815b8cf58d42f5518798bab2e7fb1286
Maia <maia.mcc@gmail.com>
2015-06-29 21:45:20 -0400

    can get all valid words starting from a trie in the middle (each trie knows its own prefix)

--- trie.py
+++ trie.py
@@ -1,6 +1,7 @@
 class Trie():
-    def __init__(self, key=""):
+    def __init__(self, key="", parent=None):
         self.key = key
+        self.parent = parent
         self.children = {}
         self.terminates = False
 
@@ -14,29 +15,52 @@ class Trie():
             if first_letter in self.children:
                 self.children[first_letter].add_word(rest_of_wd)
             else:
-                new_trie = Trie(first_letter)
+                new_trie = Trie(first_letter, self)
                 new_trie.add_word(rest_of_wd)
                 self.children[first_letter] = new_trie
 
-    def get_all_completions(self, wds_so_far=None, letters_so_far=None):
+    def get_all_completions(self, wds_so_far=None, letters_so_far=None, prefix=None):
         """Returns all possible completions of the given word."""
         if wds_so_far is None:
             wds_so_far = []
         if letters_so_far is None:
             letters_so_far = ""
+        if prefix is None:
+            prefix = self.get_parent_letters()
 
         if self.terminates:
-            wds_so_far.append(letters_so_far)
+            wds_so_far.append(prefix + letters_so_far)
 
         for child in self.children.values():
-            child.get_all_completions(wds_so_far, letters_so_far + child.key)
+            child.get_all_completions(wds_so_far, letters_so_far + child.key, prefix)
 
         return wds_so_far
 
     def get_sub_trie(self, wd):
+        """This... gets the trie that's the endpoint of the given word/prefix/string?
+        I guess? Past Maia, what were you trying to do with this function?"""
         if len(wd) < 1:
             return self
         else:
             first_letter = wd[:1]
             rest_of_wd = wd[1:]
             return self.children[first_letter].get_sub_trie(rest_of_wd)
+
+    def get_parent_letters(self, letters_so_far=None):
+        """Given a trie, returns a string of its parents (meaning, all of the
+            word that has been spelled leading up to this trie)."""
+        if letters_so_far is None:
+            letters_so_far = ""
+        if self.parent is None:
+            return letters_so_far
+        else:
+            letters_so_far = self.key + letters_so_far
+            return self.parent.get_parent_letters(letters_so_far)
+
+def make():
+    """A silly utility func. to make a trivial trie for testing. Ooh, alliteration."""
+    t = Trie()
+    for wd in ["hell", "hello", "help", "helm", "helmet"]:
+        t.add_word(wd)
+    return t
+
