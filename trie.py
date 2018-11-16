680892d497ed1f29eb0458af2876426bd78c1b27
Maia <maia.mcc@gmail.com>
2018-11-16 09:35:19 -0500

    trie fixes and tests

--- trie.py
+++ trie.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Trie:
     # TODO: enforce fixed-length Trie?
     def __init__(self, key="", parent=None):
@@ -19,7 +21,18 @@ class Trie:
 
         return '\n'.join(lns)
 
-    def add_word(self, wd):
+    def _get_node_for_wd(self, wd: str):
+        """Find the node that represents the given word."""
+        if len(wd) == 0:
+            return self
+
+        next_node = self.children.get(wd[0])
+        if not next_node:
+            return None
+
+        return next_node._get_node_for_wd(wd[1:])
+
+    def add(self, wd):
         """Add a word to the trie (creating any necessary sub-tries)."""
         if len(wd) < 1:
             self.terminates = True
@@ -30,7 +43,31 @@ class Trie:
             if first_letter not in self.children:
                 self.children[first_letter] = Trie(first_letter, self)
 
-            self.children[first_letter].add_word(rest_of_wd)
+            self.children[first_letter].add(rest_of_wd)
+
+    def contains(self, wd):
+        """Check if `wd` is a valid word that exists in this Trie."""
+        # I believe this func will work the same given a string or a letter-list.
+        # Cuz who needs type safety, amirite?
+        if len(wd) == 0:
+            return self.terminates  # is this trie the end of a word?
+
+        next_node = self.children.get(wd[0])
+        if not next_node:
+            return False
+
+        return next_node.contains(wd[1:])
+
+    def word_so_far(self, letters_so_far=None):
+        """Given a node, returns a string of its parents (meaning, all of the
+            word that has been spelled so far leading up to this node)."""
+        if letters_so_far is None:
+            letters_so_far = ""
+        if self.parent is None:  # made it to the top
+            return letters_so_far
+        else:
+            letters_so_far = self.key + letters_so_far
+            return self.parent.word_so_far(letters_so_far)
 
     def get_all_completions(self, wds_so_far=None, letters_so_far=None, prefix=None):
         """Returns all possible words reachable from the current node."""
@@ -39,7 +76,7 @@ class Trie:
         if letters_so_far is None:
             letters_so_far = ""
         if prefix is None:
-            prefix = self.get_parent_letters()
+            prefix = self.word_so_far()
 
         if self.terminates:
             wds_so_far.append(prefix + letters_so_far)
@@ -58,29 +95,6 @@ class Trie:
             first_letter = wd[:1]
             rest_of_wd = wd[1:]
             return self.children[first_letter].get_sub_trie(rest_of_wd)
-
-    def get_parent_letters(self, letters_so_far=None):
-        """Given a node, returns a string of its parents (meaning, all of the
-            word that has been spelled so far leading up to this node)."""
-        if letters_so_far is None:
-            letters_so_far = ""
-        if self.parent is None:
-            return letters_so_far
-        else:
-            letters_so_far = self.key + letters_so_far
-            return self.parent.get_parent_letters(letters_so_far)
-
-    def valid_word(self, word):
-        """If Trie contains the given word (i.e. it's a valid word), return True."""
-        # I believe this func will work the same given a string or a letter-list.
-        # Cuz who needs type safety, amirite?
-        current_node = self
-        for i, letter in enumerate(word):
-            current_node = current_node.children.get(letter)
-            if not current_node:
-                return False
-            if i == (len(word) - 1): # last letter
-                return current_node.terminates
 
     def get_options(self, word):
         """Given an incomplete word that probably contains some blanks, traverse the trie
@@ -112,12 +126,22 @@ class Trie:
                 print("whoops, no valid entries")
                 return None
 
+
+def trie_from_words(wds: List[str]) -> Trie:
+    t = Trie()
+    for wd in wds:
+        t.add(wd)
+
+    return t
+
+
 def make():
     """A silly utility func. to make a trivial trie for testing. Ooh, alliteration."""
     t = Trie()
     for wd in ["hell", "hello", "help", "helm", "helmet", "heal", "howl", "hole"]:
-        t.add_word(wd)
+        t.add(wd)
     return t
+
 
 def flatten(li):
     """Flattens a nested list, removes None entries."""
--- trie_test.py
+++ trie_test.py
@@ -1,11 +1,43 @@
-from trie import Trie
+from trie import Trie, trie_from_words
 
 
-def test_trie():
+def test_add():
     t = Trie()
-    t.add_word("cat")
-    t.add_word("car")
-    t.add_word("bat")
-    t.add_word("baal the destroyer")
+    t.add("cot")
+    t.add("car")
+    t.add("cart")
 
-    print(t)
+    assert len(t.children) == 1
+    c_trie = t.children['c']
+    assert len(c_trie.children) == 2
+    a_trie = c_trie.children['a']
+    assert len(a_trie.children) == 1
+    r_trie = a_trie.children['r']
+    assert len(r_trie.children) == 1
+    assert r_trie.terminates  # valid word ends here ("car")
+    t_trie = r_trie.children['t']
+    assert len(t_trie.children) == 0
+    assert t_trie.terminates  # valid word ends here ("cart")
+
+
+def test_contains():
+    wds = ["apple", "bear", "beer", "beers"]
+    t = trie_from_words(wds)
+
+    assert not t.contains("applet")
+    assert not t.contains("app")
+    for wd in wds:
+        assert t.contains(wd)
+
+
+def test_get_parents():
+    t = trie_from_words(["abcdefg", "hijglmnop"])
+    child = t._get_node_for_wd("abcd")
+    so_far = child.word_so_far()
+
+    assert so_far == "abcd"
+
+
+
+
+
