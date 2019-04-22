03f0ebaf9aff07e5bc6d818cb7d89334c922199b
Maia <maia.mcc@gmail.com>
2019-04-21 20:41:33 -0400

    main.py for playing around

---
+++ main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import nltk
+from nltk.corpus import wordnet as wn
+
+from board import new_board
+from solve import solve
+from trie import trie_from_words
+
+
+def all_four_letter_words():
+    try:
+        all_words = wn.words()
+        print("have wordnet, all is well")
+    except LookupError:
+        print("don't have wordnet, downloading")
+        nltk.download('wordnet')
+        all_words = wn.words()
+    return [wd for wd in all_words if len(wd) == 4 and all([ch.isalpha() for ch in wd])]
+
+
+def main():
+    vocab = all_four_letter_words()
+    wds = trie_from_words(vocab)
+
+    pattern = [
+        [True, True, True, True],
+        [True, True, True, True],
+        [True, True, True, True],
+        [True, True, True, True],
+    ]
+    b = new_board(pattern)
+
+    solved = solve(wds, b)
+    if not solved.solved:
+        raise Exception("Could not solve board:\n{}".format(str(solved)))
+
+    print("SOLVED BOARD!")
+    print()
+    print(solved)
+
+
+if __name__ == "__main__":
+    main()
